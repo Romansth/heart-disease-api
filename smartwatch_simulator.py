185be0b2b099dc7a7098e40f81fb97ccdbb965f8
Roman <roman.shrestha312@gmail.com>
2021-08-08 10:30:36 +0545

    feat: user and key added

--- smartwatch_simulator.py
+++ smartwatch_simulator.py
@@ -1,12 +1,18 @@
+from django.views.decorators import csrf
 import requests
 import time
 import random
+
+key = "YOUR_KEY_HERE"
+username = "scifidemon"
+
+data = {"username": username, "key": key}
 
 while True:
     heartBeat = random.randint(50,150)
     bp = random.randint(80,250)
 
-    requests.get(f"http://localhost:8000/patient/bp/{bp}")
-    requests.get(f"http://localhost:8000/patient/heart-rate/{heartBeat}")
+    requests.post(f"http://127.0.0.1:8000/patient/bp/{bp}", json=data)
+    requests.post(f"http://127.0.0.1:8000/patient/heart-rate/{heartBeat}", json=data)
     print(heartBeat, bp)
     time.sleep(1)
